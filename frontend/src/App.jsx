d0f4fc50fc79022e56c6f7b46b493fa185b318a3
hasna-backer <hasnabacker25@gmail.com>
2024-09-11 09:49:45 +0530

    added routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Login from "./pages/login";
+import Home from "./pages/home";
+import Signup from "./pages/signup";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
